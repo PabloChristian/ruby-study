32b8b16de36cd8ab4f8805994ec9ac5c2c5908c2
pablochristian <snakepablo@hotmail.com>
2022-10-12 13:35:04 -0300

    chore: initial files and project

---
+++ array.rb
@@ -0,0 +1,9 @@
+address = [1,2,3,4,5,6,7,8,9]
+p address
+new_address = address.reverse!
+p new_address
+
+#say_hello("teste pablo")
+#def say_hello(text)
+  #puts text
+#end
---
+++ method.rb
@@ -0,0 +1,4 @@
+def say_hello(text)
+  puts text
+end
+say_hello "teste pablo"
