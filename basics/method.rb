4ffc5d3e94ba4b293ff6ac09d7df98cd46a3b24d
pablochristian <snakepablo@hotmail.com>
2022-12-09 17:45:03 -0300

    feat: add static method

--- basics/method.rb
+++ basics/method.rb
@@ -2,3 +2,12 @@ def say_hello(text)
   puts text
 end
 say_hello "teste pablo"
+
+
+#static methods
+#they have self notation and inside a class
+class Ask
+  def self.make_permalink(phrase)
+    phrase.strip.downcase.gsub! /\ +/, '-'
+  end
+end
