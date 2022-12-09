8143a297f9151b4cbdd6b3938641e5f5afb734f4
pablochristian <snakepablo@hotmail.com>
2022-12-09 17:48:36 -0300

    feat: for with count

--- data-structures/for.rb
+++ data-structures/for.rb
@@ -2,3 +2,13 @@ address = [1,2,3,4,5,6,7,8,9]
 for value in address
   puts value
 end
+
+##de 0 a 5
+for i in 0..5
+  puts "Value of local variable is #{i}"
+end
+
+#array length
+for i in array.length()
+  puts "Value of local variable is #{i}"
+end
